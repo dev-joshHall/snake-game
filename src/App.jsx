a00a4badeb7d85a0af121c42377526accc9cef8d
dev-joshHall <63134862+dev-joshHall@users.noreply.github.com>
2022-06-26 22:47:25 -0600

    added snake canvas

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SnakeCanvas from "./components/snake-canvas";
+
+function App() {
+  return (
+    <div className="App">
+      <SnakeCanvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/snake-canvas.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SnakeCanvas = () => {
+  return (
+    <iframe
+      title={"Snake Game"}
+      src={"https://editor.p5js.org/jhall001/full/qJFCu42l6"}
+      width={"420px"}
+      height={"450px"}
+      frameBorder={"0"}
+      scrolling={"no"}
+      ponterEvents={"none"}
+    ></iframe>
+  );
+};
+
+export default SnakeCanvas;
