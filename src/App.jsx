73bed8a764d80eec79e47bf61e6cc8f194e4fe91
dev-joshHall <63134862+dev-joshHall@users.noreply.github.com>
2022-06-29 17:19:44 -0600

    Added play button and alt controls for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import "./App.css";
 import SnakeCanvas from "./components/snake-canvas";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <SnakeCanvas />
-    </div>
-  );
+class App extends Component {
+  state = { mode: "arrows" };
+
+  changeMode = () => {
+    this.state.mode === "normal"
+      ? this.setState({ mode: "arrows" })
+      : this.setState({ mode: "normal" });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <SnakeCanvas mode={this.state.mode} changeMode={this.changeMode} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/snake-canvas.jsx
+++ src/components/snake-canvas.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
 import Sketch from "react-p5";
+import AlternateControls from "./alternate-controls";
 
 const SnakeCanvas = (props) => {
   var bgImg;
+
+  var gameStarted = false;
 
   var snake = {
     pieces: [
@@ -21,11 +24,17 @@ const SnakeCanvas = (props) => {
   };
 
   const changeFood = () => {
+    if (!gameStarted) {
+      return;
+    }
     food.x = Math.floor(Math.random() * 20) * 20;
     food.y = Math.floor(Math.random() * 20) * 20;
   };
 
   const checkEaten = () => {
+    if (!gameStarted) {
+      return;
+    }
     var head = snake.pieces[0];
     if (head.x === food.x && head.y === food.y) {
       return true;
@@ -34,6 +43,9 @@ const SnakeCanvas = (props) => {
   };
 
   const checkWallCollide = () => {
+    if (!gameStarted) {
+      return;
+    }
     var head = snake.pieces[0];
     if (head.x >= 400 || head.x < 0) {
       return true;
@@ -45,6 +57,9 @@ const SnakeCanvas = (props) => {
   };
 
   const checkSelfCollide = () => {
+    if (!gameStarted) {
+      return;
+    }
     var head = snake.pieces[0];
     for (let i = 1; i < snake.pieces.length; i++) {
       if (head.x === snake.pieces[i].x && head.y === snake.pieces[i].y) {
@@ -55,26 +70,47 @@ const SnakeCanvas = (props) => {
   };
 
   const setup = (p5, canvasParentRef) => {
-    p5.createCanvas(400, 400).parent(canvasParentRef);
+    var cnv = p5.createCanvas(400, 400).parent(canvasParentRef);
+    cnv.mouseClicked(mc);
     p5.frameRate(5);
     bgImg = p5.loadImage(require("./leaves.png"));
   };
 
+  const displayStart = (p5) => {
+    p5.fill(200, 10, 20);
+    p5.rect(100, 150, 200, 100, 20);
+    p5.fill(255);
+    p5.textSize(30);
+    p5.text("PLAY NOW!", 116, 210);
+  };
+
   useEffect(() => {
     const keyDownHandler = (event) => {
-      if (event.key === "ArrowLeft" && snake.xvel !== 20) {
+      if (!gameStarted) {
+        return;
+      }
+      if (
+        (event.key === "ArrowLeft" || event.key === "a") &&
+        snake.xvel !== 20
+      ) {
         snake.xvel = -20;
         snake.yvel = 0;
       }
-      if (event.key === "ArrowRight" && snake.xvel !== -20) {
+      if (
+        (event.key === "ArrowRight" || event.key === "d") &&
+        snake.xvel !== -20
+      ) {
         snake.xvel = 20;
         snake.yvel = 0;
       }
-      if (event.key === "ArrowUp" && snake.yvel !== 20) {
+      if ((event.key === "ArrowUp" || event.key === "w") && snake.yvel !== 20) {
         snake.xvel = 0;
         snake.yvel = -20;
       }
-      if (event.key === "ArrowDown" && snake.yvel !== -20) {
+      if (
+        (event.key === "ArrowDown" || event.key === "s") &&
+        snake.yvel !== -20
+      ) {
         snake.xvel = 0;
         snake.yvel = 20;
       }
@@ -85,8 +121,57 @@ const SnakeCanvas = (props) => {
     };
   });
 
+  const leftBtnPressed = () => {
+    if (!gameStarted) {
+      return;
+    }
+    if (snake.xvel !== 20) {
+      snake.xvel = -20;
+      snake.yvel = 0;
+    }
+  };
+
+  const rightBtnPressed = () => {
+    if (!gameStarted) {
+      return;
+    }
+    if (snake.xvel !== -20) {
+      snake.xvel = 20;
+      snake.yvel = 0;
+    }
+  };
+
+  const upBtnPressed = () => {
+    if (!gameStarted) {
+      return;
+    }
+    if (snake.yvel !== 20) {
+      snake.xvel = 0;
+      snake.yvel = -20;
+    }
+  };
+
+  const downBtnPressed = () => {
+    if (!gameStarted) {
+      return;
+    }
+    if (snake.yvel !== -20) {
+      snake.xvel = 0;
+      snake.yvel = 20;
+    }
+  };
+
+  const mc = () => {
+    gameStarted = true;
+  };
+
   const draw = (p5) => {
     p5.background(bgImg);
+    if (!gameStarted) {
+      displayStart(p5);
+      return;
+    }
+
     if (checkWallCollide() || checkSelfCollide()) {
       p5.fill(255); // change text color to white
       p5.text("Game Over", 130, 180);
@@ -113,14 +198,27 @@ const SnakeCanvas = (props) => {
     snake.pieces.unshift({ x: head.x + snake.xvel, y: head.y + snake.yvel });
     snake.pieces.pop(); // delete the tail
 
-    p5.rect(food.x, food.y, 20, 20);
+    p5.fill(200, 10, 20);
+    p5.ellipse(food.x + 10, food.y + 10, 20, 20);
 
     p5.fill(255);
     p5.text("Score: " + snake.score, 10, 20);
     p5.textSize(25);
   };
 
-  return <Sketch setup={setup} draw={draw} />;
+  return (
+    <React.Fragment>
+      <Sketch setup={setup} draw={draw} />
+      <AlternateControls
+        mode={props.mode}
+        changeMode={props.changeMode}
+        onRight={rightBtnPressed}
+        onLeft={leftBtnPressed}
+        onUp={upBtnPressed}
+        onDown={downBtnPressed}
+      />
+    </React.Fragment>
+  );
 };
 
 export default SnakeCanvas;
